954bd46d353b9df38e7f0f00b7906f54290a749b
Gautamraju-286 <mudunurigautamraju.25.csm@anits.edu.in>
2026-09-11 11:54:55 +0530

    Add files via upload

---
+++ Assignment/Assignment 3/case_convertion.java
@@ -0,0 +1,10 @@
+public class case_convertion {
+    public static void main(String[] args) {
+        String name = "gautam";
+        String upperCaseName = name.toUpperCase();
+        System.out.println("to upper case: " + upperCaseName);
+        String lowerCaseName = name.toLowerCase();
+        System.out.println("to lower case: " + lowerCaseName);
+
+    }
+}
