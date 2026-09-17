abaa288351298862f16304520dd0203e5e9e5374
Gautamraju-286 <mudunurigautamraju.25.csm@anits.edu.in>
2026-09-17 21:46:28 +0530

    equals.java

---
+++ Practice programs/equals.java
@@ -0,0 +1,8 @@
+public class EqualsExample {
+    public static void main(String[] args) {
+        String name1 = "Gautam";
+        String name2 = "Gautam";
+
+        System.out.println(name1.equals(name2));
+    }
+}
