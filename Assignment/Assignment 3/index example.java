561170b2483ced5bd8151f4caf59d125a6c5abc7
Gautamraju-286 <mudunurigautamraju.25.csm@anits.edu.in>
2026-09-17 20:32:52 +0530

    index example.java

---
+++ Assignment/Assignment 3/index example.java
@@ -0,0 +1,7 @@
+public class IndexExample {
+    public static void main(String[] args) {
+        String name = "gautam";
+        int result = name.indexOf("a");
+        System.out.println("result is " + result);
+    }
+}
