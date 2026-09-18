384c2cf81797856cd334f0695f18c6262c72ac2a
Gautamraju-286 <mudunurigautamraju.25.csm@anits.edu.in>
2026-09-18 09:44:01 +0530

    length.java

---
+++ Assignment/Assignment 3/length.java
@@ -0,0 +1,8 @@
+public class LengthExample {
+    public static void main(String[] args) {
+        String name = "Gautam";
+        int result = name.length();
+
+        System.out.println("Length is: " + result);
+    }
+}
